47379a9e0e2ca4ae9e8878f6877a53385582e978
KatieTown <87946461+KatieTown@users.noreply.github.com>
2021-10-10 22:08:56 +0100

    Update Plastics.R

--- Plastics.R
+++ Plastics.R
@@ -1,9 +1,6 @@
 library(tidyverse)
 library(reshape2)
 library(scales)
-install.packages("ggthemes")
-library(ggthemes)
-vignette("pivot")
 
 plastics <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-26/plastics.csv')
 
